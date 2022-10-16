482d9ae67b52c2d0e6b69436adf64a6ad4a356bd
Syuan Tsai <carsun00@gmail.com>
2022-10-16 17:09:02 +0800

    Feat 初始化

---
+++ DesignPattern/Tests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace DesignPattern
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
